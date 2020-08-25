18cff231230099749b8f63ed5e171e62d82f0452
Vincent Pacheco <vincentpacheco@outlook.com>
2020-08-25 14:52:48 +0200

    tables controller OK

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,19 +1,53 @@
 class TablesController < ApplicationController
   def index
-  end
-
+  znd
+  
   def show
+    @restaurant = Restaurant.find(params[:id])
+    @order = Order.new
+    @orders = @restaurant.orders.where(user: current_user)
+    authorize @restaurant
   end
 
   def new
+    @restaurant = current_user.restaurants.new
+    authorize @restaurant
   end
 
   def create
-  end
-
-  def update
+    @restaurant = restaurant.new(restaurant_params)
+    authorize @restaurant
+    @restaurant.user = current_user
+    if @restaurant.save!
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
-end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+    @restaurant.update(restaurant_params)
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:title, :description, :longdescription, :price, :photo, :address, :capacity, :duration)
+  end
