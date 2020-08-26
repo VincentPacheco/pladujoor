66f5a51a819a2e9942f9344a737571d5506b54b5
Stéphane ZE-OGIER <contact@ze-ogier.com>
2020-08-26 08:58:16 +0200

    [controller] add menu controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,53 @@
 class MenusController < ApplicationController
+  def index
+    @menus = policy_scope(Menu)
+  end
+
   def show
     @menu = Menu.find(params[:id])
     authorize @menu
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @menu = Menu.new
+    authorize @menu
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @menu = Menu.new(order_params)
+    @menu.restaurant = @restaurant
+    if @menu.save!
+      redirect_to restaurant_menus_path
+    authorize @menu
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    authorize @order
+    @order.destroy
+    redirect_to orders_path
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+    authorize @order
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    authorize @order
+    @order.update(order_params)
+    redirect_to order_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:menu).permit(:name)
   end
 end
