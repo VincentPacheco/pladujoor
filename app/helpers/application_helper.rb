34ef8d543d866005d376a9bd75a97f45bb02e725
Raphael Bilon <raphael.bilon@essec.edu>
2020-08-31 18:55:12 +0200

    fin de journée

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def cart_index?
+    controller_name == 'orders' && action_name == 'confirmation' || controller_name == 'orders' && action_name == 'show'
+  end
+
+  def menu_index?
+    controller_name == 'dishes' && action_name == 'show' || controller_name == 'orders' && action_name == 'new'
+  end
 end
